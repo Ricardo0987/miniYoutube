d3159e27c13cefbe60b8cb6a2f037ed401a5dcd4
ricardoqr123 <ricardo.ospina@qrvey.com>
2021-10-05 22:45:41 -0500

    create layout with router

---
+++ client/src/components/Layout.js
@@ -0,0 +1,30 @@
+import { Container, CssBaseline, Grid } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter, Route } from "react-router-dom";
+import VideoList from "./videos/videoList";
+
+const layoutStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: 80,
+  },
+}));
+
+const Layout = () => {
+  const classes = layoutStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container>
+        <Grid container className={classes.container}>
+          <BrowserRouter>
+            <Route exact path="/videos" component={VideoList} />
+          </BrowserRouter>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
