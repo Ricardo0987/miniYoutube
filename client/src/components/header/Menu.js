732f060cbcffed1baa2ec3ddbd510ce8c2a3bef3
ricardoqr123 <ricardo.ospina@qrvey.com>
2021-10-10 20:35:01 -0500

    create and update videolist and menu

---
+++ client/src/components/header/Menu.js
@@ -0,0 +1,46 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { reactLocalStorage } from "reactjs-localstorage";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+export default function Menu() {
+  const classes = useStyles();
+  let history = useHistory();
+
+  const goSigIn = () => {
+    reactLocalStorage.remove("token");
+    history.push("/signin");
+  };
+
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Videos
+          </Typography>
+          <Button color="inherit" onClick={goSigIn}>
+            LogOut
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ client/src/components/videos/videoList.js
@@ -0,0 +1,168 @@
+import React, { useEffect, useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { CONFIG } from "../../config";
+import axios from "axios";
+import Chip from "@material-ui/core/Chip";
+import Menu from "../header/Menu";
+import Badge from "@material-ui/core/Badge";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import Swal from "sweetalert2";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  card: {
+    minWidth: 275,
+    margin: 10,
+    padding: 5,
+  },
+  chips: {
+    marginLeft: 2,
+  },
+  like: {
+    marginTop: 22,
+  },
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+    margin: 4,
+  },
+  top: {
+    fontSize: 30,
+    textAlign: "center",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "#b5b5b5",
+    margin: 4,
+    padding: "10px",
+    borderRadius: "20px",
+  },
+});
+
+export default function VideoList() {
+  const classes = useStyles();
+
+  const [videos, setVideos] = useState([]);
+  const [user, setuser] = useState("61631465a13ef518b1057995");
+
+  const getVideos = async () => {
+    await axios.get(CONFIG.HOST + "/videos/").then((response) => {
+      setVideos(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const addvideo = (e) => {
+    e.preventDefault();
+    Swal.fire({
+      title: "Add video",
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="name of video"/>' +
+        '<input id="swal-input2" class="swal2-input" placeholder="tags of video"/>' +
+        '<input id="swal-input3" class="swal2-input" placeholder="file"/>',
+      focusConfirm: false,
+      showCancelButton: true,
+      preConfirm: () => {
+        const video = {
+          name: document.getElementById("swal-input1").value,
+          tags: document.getElementById("swal-input2").value.split(","),
+          file: document.getElementById("swal-input3").value,
+          idUser: user,
+        };
+        return video;
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.post(CONFIG.HOST + "/video/", result.value).then((response) => {
+          console.log(response);
+          getVideos();
+        });
+      }
+    });
+  };
+
+  const toggleLike = async (video) => {
+    await axios.post(CONFIG.HOST + "/like/", { idUser: user, idVideo: video._id }).then((response) => {
+      getVideos();
+    });
+  };
+
+  const handleChangeUser = (e) => {
+    const { value } = e.target;
+    setuser(value);
+  };
+
+  return (
+    <div>
+      <div className={classes.top}>
+        <form noValidate autoComplete="off">
+          <Button variant="contained" color="secondary" onClick={addvideo}>
+            Add Video
+          </Button>
+        </form>
+        <TextField
+          size="small"
+          variant="filled"
+          label="UserId"
+          placeholder="idUser"
+          name="idUser"
+          value="61630453f19c950c42146836" //FIXME: quitar
+          onChange={handleChangeUser}
+        />
+        <FormControl className={classes.search}>
+          <InputLabel htmlFor="input-with-icon-adornment">Search Video</InputLabel>
+          <Input
+            startAdornment={
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
+      <div className={classes.container}>
+        <Menu />
+        {videos.map((video) => (
+          <Card key={video.id} className={classes.card}>
+            <div className={classes.details}>
+              <CardContent className={classes.content}>
+                <Typography component="h2" variant="h6" className={classes.title}>
+                  {video.name}
+                </Typography>
+                Tags:
+                {video.tags.map((tag) => (
+                  <Chip label={tag} color="primary" className={classes.chips} />
+                ))}
+                <Typography component="h2" variant="h6" className={classes.like}>
+                  <Badge color="secondary" badgeContent={video.likes} showZero>
+                    <ThumbUpAltIcon fontSize="large" onClick={() => toggleLike(video)} />
+                  </Badge>
+                </Typography>
+              </CardContent>
+            </div>
+            <video width="320" height="240" controls>
+              <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
